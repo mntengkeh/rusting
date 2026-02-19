aa316f1f00600d610e6e180048bc0bf246c3fc38
21 <maxwellntengkeh@gmail.com>
2026-02-19 22:37:25 +0100

    feat(mntengkeh): day05 - loops exercise 2

--- rust-exercises/students/mntengkeh/part1/03_loops/solution.rs
+++ rust-exercises/students/mntengkeh/part1/03_loops/solution.rs
@@ -19,25 +19,42 @@ fn exercise_1() {
 		}
 }
 
-// Exercise 2
+// Exercise2
 fn exercise_2() {
-
+		let mut increment = 0;
+		let mut x = 2;
+		let target = loop {
+			if x == 4 {
+				increment += 5;
+			} else {
+				if increment < 5 {
+					x += 1;
+					continue;
+				}
+				if x % 7 == 0 {
+					break x;
+				} else {
+					increment += 2;
+				}
+			}
+			x += increment;
+		};
+		println!("First number > 7 and is a perfect square: {}", target);
 }
 
 // Exercise 3
-fn exercise_3() {
-		println!("Multiplication Table\n");
+fn exercise_3(){
+	println!("Multiplication Table\n");
 		for i in 1..6 {
 			for j in 1..6 {
 				println!("{} x {} = {}", i, j, i * j);
 			}
 			println!("\n");
 		}
-		
 }
 
 fn main() {
     //exercise_1();
     exercise_2();
-    exercise_3();
+    //exercise_3();
 }

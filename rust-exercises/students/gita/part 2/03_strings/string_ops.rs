80a9db68c94205f202fc32032d04ff7918a93e54
iws-seed <iwstechnical7@gmail.com>
2026-03-04 10:31:08 +0100

    done with string concatenation exercise_2: format! replacing stringsn

--- rust-exercises/students/gita/part 2/03_strings/string_ops.rs
+++ rust-exercises/students/gita/part 2/03_strings/string_ops.rs
@@ -5,6 +5,7 @@ let s=String::from("Initialized content");
 let s="initial content".to_string(); //same as above
 appending_string();
 concatenating_string();
+replacing_part_string();
 
 }
 
@@ -32,9 +33,16 @@ fn concatenating_string() {
 fn multiple_concatenations(){
     let s1=String::from("tic");
     let s2=String::from("tac");
-    let s3=Srting::from("toe");
+    let s3=String::from("toe");
     // multiple concatenation use  format! macro
     let s=format!("{}-{}-{}", s1, s2, s3); //Doesn't take ownership
     println!("{}", s); //tic tac toe
     println!("{}", s1); //still exist , because its ownership is nor taken
+}
+
+fn replacing_part_string() {
+    let s=String::from("I like apples!");
+    let new_s=s.replace("apples", "oranges");
+    println!("{}", new_s); //I like ornages
+
 }

79bedcccf65c55fb6601bd4969b0ff33e9a026a6
iws-seed <iwstechnical7@gmail.com>
2026-03-05 12:56:37 +0100

    started exploring vectors growable list->accessing elements

--- rust-exercises/students/gita/part 2/04_vectors/practice.rs
+++ rust-exercises/students/gita/part 2/04_vectors/practice.rs
@@ -3,6 +3,7 @@ fn main() {
     let v:Vec<i32>=Vec::new(); //Empty vector, must specify type
     let v=vec![1, 2, 3, 4]; //vec! macro infers type from content
     adding_elements();
+    accessing_elements();
 
 
 }
@@ -14,4 +15,16 @@ fn adding_elements() {
     v.push(7);
     v.push(8);
     println!("{:?}", v);
+}
+
+fn accessing_elements() {
+    let v=vec![1, 2, 3, 4, 5];
+    // using indexing:
+    let third=v[2];
+    println!("The third element is {}", third);
+    // method 2: using get (returns Option, safer)
+    match v.get(2) {
+        Some(answer)=>println!("The answer is : {}", answer),
+        None=>println!("There is no third element"),
+    }
 }

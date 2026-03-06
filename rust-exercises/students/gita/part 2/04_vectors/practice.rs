59af800d4bb4e7b5f2468d259336d91d1c8fa524
iws-seed <iwstechnical7@gmail.com>
2026-03-06 08:21:14 +0100

    (vectors)-iterating over vectors

--- rust-exercises/students/gita/part 2/04_vectors/practice.rs
+++ rust-exercises/students/gita/part 2/04_vectors/practice.rs
@@ -1,15 +1,14 @@
 fn main() {
     // creating vectors
-    let v:Vec<i32>=Vec::new(); //Empty vector, must specify type
-    let v=vec![1, 2, 3, 4]; //vec! macro infers type from content
+    let v: Vec<i32> = Vec::new(); //Empty vector, must specify type
+    let v = vec![1, 2, 3, 4]; //vec! macro infers type from content
     adding_elements();
     accessing_elements();
-
-
+    iterating_over_vectors();
 }
 
 fn adding_elements() {
-    let mut v=Vec::new();
+    let mut v = Vec::new();
     v.push(5); //compiler infers v is Vec<i32> from this
     v.push(6);
     v.push(7);
@@ -18,23 +17,47 @@ fn adding_elements() {
 }
 
 fn accessing_elements() {
-    let v=vec![1, 2, 3, 4, 5];
+    let v = vec![1, 2, 3, 4, 5];
     // using indexing:
-    let third=v[2];
+    let third = v[2];
     println!("The third element is {}", third);
     // method 2: using get (returns Option, safer)
     match v.get(2) {
-        Some(answer)=>println!("The answer is : {}", answer),
-        None=>println!("There is no third element"),
+        Some(answer) => println!("The answer is : {}", answer),
+        None => println!("There is no third element"),
     }
 }
 
-fn mutable_borrow(){
-    let mut v=vec![1, 2, 3, 4, 5];
-    let first=&v[0]; //Immutable borrow of the vector
-    v.push(6); //Error, can mutably borrow while immutably borrowed
+fn mutable_borrow() {
+    let mut v = vec![1, 2, 3, 4, 5];
+    let first = &v[0]; //Immutable borrow of the vector
+    //v.push(6); //Error, can mutably borrow while immutably borrowed
     println!("The first element is: {}", first);
     // now first is no longer in use, so we can modify it
     v.push(6);
     println!("{:?}", v);
+}
+
+// iteratting over vectors
+fn iterating_over_vectors() {
+    let v = vec![30, 32, 100];
+    // immutable iteration
+    for i in &v {
+        println!("{}", i);
+    }
+
+    // Mutable iteration
+    let mut v = vec![20, 34, 34, 56];
+    // explain dereferencing:the * operator is used to dereference a mutable reference, allowing us to modify the value it points to. In this case, *i gives us access to the actual integer value in the vector, which we can then modify by adding 50 to it.
+    for i in &mut v {
+        *i += 50;
+    }
+    println!("{:?}", v); //
+
+    // Taking ownership, vectors become unusable after
+    for element in v {
+        println!("{}", element);
+    }
+
+    //println!("{:?}", v); // Error! v has been moved into the for loop
 }
